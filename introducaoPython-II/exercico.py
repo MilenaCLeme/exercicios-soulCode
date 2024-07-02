92a80d412e1874c7f7a7ae4ae4ccfd8a5f8eb4bc
Milena Leme <milenaleme4@hotmail.com>
2024-07-02 19:22:48 -0300

    exercise finished

---
+++ introducaoPython-II/exercico.py
@@ -0,0 +1,27 @@
+"""
+
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+
+1  - Crie quatro variáveis, sendo elas com nomes de animais, quantidade de animais, peso dos animais e se eles são vacinados ou não. 
+
+2 - Comente seu código usando comentários longos e curtos.
+
+"""
+
+# exercicio 1
+
+# animais nomes
+dog = "Sarah"
+cat = "Mia"
+
+# quantidade de animais
+many_dog = 10
+many_cat = 15
+
+# peso de sarah e mia 
+weight_dog_sarah = 10.4
+weight_cat_mia = 6.0
+
+# são vacimados ou não
+vaccinated_sarah = False
+vaccinated_mia = True
