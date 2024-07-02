e505453ff14b1232f836cb977bddf7f1296c97da
Milena Leme <milenaleme4@hotmail.com>
2024-07-02 19:15:35 -0300

    class finished about type var

--- introducaoPython-II/variaveis.py
+++ introducaoPython-II/variaveis.py
@@ -6,3 +6,10 @@ nome = "Milena"
 peso2 = "65.4"
 situacao = True
 situacao2 = False
+
+#Tipos de dados
+
+print(type(idade)) # <class 'int'>
+print(type(peso)) # <class 'float'>
+print(type(nome)) # <class 'str'> 
+print(type(situacao))
