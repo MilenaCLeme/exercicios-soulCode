7dc2beaabddb2586f29c2e1f664c301685e874f5
Milena Leme <milenaleme4@hotmail.com>
2024-07-02 20:13:06 -0300

    exercise about class manipulation data

---
+++ manipulacaoDeDados-III/exercicio.py
@@ -0,0 +1,18 @@
+"""
+
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+
+1. Crie três inputs que receba os três dados abaixo:
+    Nome do tipo string
+    Altura do tipo Float
+    CPF do tipo inteiro  
+2. Exiba através de um único print as três informações para o usuário.
+
+
+"""
+
+nome = input("Nome Completo: ")#string
+altura = input("Altura: ")#float
+cpf = input("CPF: ")#inteiro
+
+print("O nome completo do usuário",nome, ". Altura do usuario é ", altura, ".É o cpf do usuario: ", cpf, ".")
