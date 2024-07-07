7043eba882897577b6b30564ea4abe3df25f595e
Milena Leme <milenaleme4@hotmail.com>
2024-07-07 00:49:35 -0300

    class about strings and var char str

---
+++ strings-VI/introducaostrings.py
@@ -0,0 +1,24 @@
+#Introdução Strings
+
+nome = "Milena Chrysostomo Leme"
+nome2 = 'Milena Chrysostomo Leme'
+nome3 = """
+
+Esta é uma String longa
+contendo informação sobre
+Python
+
+"""
+
+nome4 = '''
+
+Esta é uma String longa
+contendo informação sobre
+Python
+
+'''
+
+print(nome)
+print(nome2)
+print(nome3)
+print(nome4)
