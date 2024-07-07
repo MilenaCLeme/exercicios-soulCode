b64007b7c8a3ec3eec5446c302ca17ce822b4d90
Milena Leme <milenaleme4@hotmail.com>
2024-07-06 23:07:10 -0300

    uploand readme and file comendoelif modified

---
+++ estruturaDeCondicao-IV/comendoelif.py
@@ -0,0 +1,13 @@
+# O comendo Elif contição extra 
+numero = 12
+
+if numero == 10:
+    print("Este número é igual a 10!")
+elif numero == 11:
+    print("Este número é igual a 11!")
+elif numero == 12:
+    print("Este número é igual a 12!")
+elif numero == 13:
+    print("Este número é igual a 13!")
+else:
+    print("Este numero não é igual nem a 10, 11, 12 ou 13!")
