c2f3223c15b4b3be7b0c3e32de9856b97ef46306
Milena Leme <milenaleme4@hotmail.com>
2024-07-07 01:20:33 -0300

    class about for, while and enumarate

---
+++ strings-VI/percorrendostrings.py
@@ -0,0 +1,15 @@
+nome = "Python"
+
+#for
+for i in nome:
+    print(i)
+
+#while
+i = 0
+while i < len(nome):
+    print(nome[i])
+    i+=1
+
+#for(numerate)
+for k, v in enumerate(nome):
+    print(k, v)
