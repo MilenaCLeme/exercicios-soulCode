602ef6d2eb9194b23f4c3eea4472a630a1e80250
Milena Leme <milenaleme4@hotmail.com>
2024-07-07 01:06:28 -0300

    class about purcharse strings

--- estruturaDeControleDeFluxo-V/lacoforrange.py
+++ estruturaDeControleDeFluxo-V/lacoforrange.py
@@ -2,3 +2,6 @@
 
 for i in range(10):
     print("Ola mundo!")
+
+for j in range(9, -1, -1):
+    print(j)
---
+++ strings-VI/comparandostring.py
@@ -0,0 +1,9 @@
+#Comparando strings
+
+print("a" == "b")
+print("a" != "b")
+print("a" > "x")
+print("a" > "1")
+
+for i in range(122):
+    print(str(i) + " - "+chr(i))
