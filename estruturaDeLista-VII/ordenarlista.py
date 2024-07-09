a9f29949452703e3268007c99c962f63e3a2757f
Milena Leme <milenaleme4@hotmail.com>
2024-07-09 16:54:37 -0300

    class about ordering lista

---
+++ estruturaDeLista-VII/ordenarlista.py
@@ -0,0 +1,12 @@
+# ordenação lista
+lista = ["a", "x", "s", "w", "d", "a", "n", "a"]
+lista.reverse()
+print(lista)
+lista.sort()
+print(lista)
+
+for i in range(len(lista)):
+    print(lista[i])
+
+l = lista.count("a")
+print(l)
