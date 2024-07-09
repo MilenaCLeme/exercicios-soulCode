8e7df98dfa3aa0c58e3ca4ce2b97e6c58fb399c9
Milena Leme <milenaleme4@hotmail.com>
2024-07-09 17:00:15 -0300

    exercise about two

---
+++ estruturaDeLista-VII/exercicio2.py
@@ -0,0 +1,17 @@
+"""
+
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+
+Crie uma lista de móveis de uma casa;
+Adicione um elemento no final da lista;
+Remova um elemento pelo seu índice.
+
+
+"""
+
+lista = ["cama", "geladeira", "sofá", "tv"]
+
+lista.append('fogão')
+print(lista)
+lista.pop(0)
+print(lista)
