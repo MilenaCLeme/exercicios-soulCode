82d2804fad6b1e6bc4c29fa77c8e210ad580c196
Milena Leme <milenaleme4@hotmail.com>
2024-07-09 16:55:07 -0300

    class about crud list

---
+++ estruturaDeLista-VII/incluiralterarexecluir.py
@@ -0,0 +1,12 @@
+# incluir, alterar, excluir
+
+animais = ['Gato', 'Cachorro', 'Elefante']
+print(animais)
+animais.append('Galinha')
+print(animais)
+animais.insert(0, 'Papagaio')
+print(animais)
+animais.pop(0)
+print(animais)
+animais.remove('Gato')
+print(animais)
