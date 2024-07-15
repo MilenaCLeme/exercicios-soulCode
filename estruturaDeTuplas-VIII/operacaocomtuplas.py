0423e989e23cba244fb35b55c255cc5a18fccb21
Milena Leme <milenaleme4@hotmail.com>
2024-07-15 15:14:12 -0300

    class about tuplas with operation

---
+++ estruturaDeTuplas-VIII/operacaocomtuplas.py
@@ -0,0 +1,7 @@
+elementos_tuplas = ('São Paulo', 'Belo Horizonte', 'Teresina')
+print('Manus' in elementos_tuplas)
+print('São Paulo' in elementos_tuplas)
+
+nomes_tuplas = ('José', 'Carlos', 'Maria', 'Pedro', 'Joana', 'Maria')
+print(nomes_tuplas.count('Maria'))
+print(nomes_tuplas.index('Carlos'))
